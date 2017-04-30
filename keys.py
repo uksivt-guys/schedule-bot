be720f041e8db9af835bb1f212eb6ac3bcb3fb72
Ruslan Sayfullin <uraltus@yandex.ru>
2017-04-30 11:07:43 +0500

    Удалены ключи

--- keys.py
+++ keys.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 #token telegram bot
 #токен для бота подписчиков
-SUBSCRIBER_TOKEN = '347098577:AAFqPkJLz39vEYsyJmWv5jx9BidEkOmXxxM'
+SUBSCRIBER_TOKEN = ''
 #токен для бота администратора
-DISPATCHER_TOKEN = '333306297:AAEovfbJpulk285cROVFVmnp12QTAr3TsMo'
+DISPATCHER_TOKEN = ''
 #путь для файла расписания
-PATH_TO_FILES = '/media/hatab/DADC3EFDDC3ED38B/project/schedule-bot/'
+PATH_TO_FILES = ''
 #ключи базы данных
 class KeysDB(object):
-	HOST     = '127.0.0.1'
-	USER     = 'root'
-	PASSWORD = '12345'
-	DATABASE = 'schedule'
+	HOST     = ''
+	USER     = ''
+	PASSWORD = ''
+	DATABASE = ''
